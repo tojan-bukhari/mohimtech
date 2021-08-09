c274db29483a84b061d84efde8a746ba0a7eb50e
tojan bukhari <gin12333@gmail.com>
2021-08-09 22:21:02 +0300

    drag and drop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 import React, { useState,useEffect } from "react";
 import axios from 'axios';
+import './App.css';
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+import _, { endsWith } from "lodash";
+import {v4} from "uuid";
 
 function App() {
   const [text, settext] = useState("");
   const [date, setdate] = useState("");
-  const [data, setdata] = useState();
   const [priority, setpriority] = useState('');
   const [select, setselect] = useState();
   const [num, setnum]= useState();
 
+  const [name, setname] = useState("")
+  const [state, setState] = useState({
+    "todo": {
+      title: "Todo",
+      items: []
+    },
+    "in-progress": {
+      title: "In Progress",
+      items: []
+    },
+    "done": {
+      title: "Completed",
+      items: []
+    }
+  })
 
-  
- 
+  const handleDragEnd = ({destination, source}) => {
+    if (!destination) {
+      return
+    }
+
+    if (destination.index === source.index && destination.droppableId === source.droppableId) {
+      return
+    }
+
+    // Creating a copy of item before removing it from state
+    const itemCopy = {...state[source.droppableId].items[source.index]}
+
+    setState(prev => {
+      prev = {...prev}
+      // Remove from previous items array
+      prev[source.droppableId].items.splice(source.index, 1)
+
+
+      // Adding to new items array location
+      prev[destination.droppableId].items.splice(destination.index, 0, itemCopy)
+
+      return prev
+    })
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -19,10 +59,21 @@ function App() {
         ///////////GET ALL TASKS /////////////
         const tasks = await axios.get('http://localhost:5000/api/task/');
         console.log('tasks', tasks.data);
-        setdata(tasks.data);
-        if(data !== undefined){
-          console.log('array of data after set',data)
-        }
+        var array = tasks.data
+        for(var i = 0; i <array.length;i++){
+        setState(prev => {
+          return {
+            ...prev,
+            todo: {
+              title: "Todo",
+              items: [
+                array[i],
+                ...prev.todo.items
+              ]
+            }
+          }
+        })}
+        
       } catch (error) {
         console.log(error, "oh nooooo");
       }
@@ -37,7 +88,19 @@ function App() {
      console.log('this is result',res.data)
      const tasks = await axios.get('http://localhost:5000/api/task/');
      console.log('tasks', tasks.data);
-     setdata(tasks.data); 
+        setState(prev => {
+          return {
+            ...prev,
+            todo: {
+              title: "Todo",
+              items: [
+                tasks.data[tasks.data.length-1],
+                ...prev.todo.items
+              ]
+            }
+          }
+        })
+     
   }
 
    /////////////DELETE DATA//////////////
@@ -47,13 +110,30 @@ function App() {
     console.log('task deleted');
     const tasks = await axios.get('http://localhost:5000/api/task/');
      console.log('tasks', tasks.data);
-     setdata(tasks.data); 
-  }
- 
+     var array = tasks.data
+        for(var i = 0; i <array.length;i++){
+        setState(prev => {
+          return {
+            ...prev,
+            todo: {
+              title: "Todo",
+              items: [
+                array[i],
+                ...prev.todo.items
+                
+              ]
+            }
+          }
+        })} 
+ }
+ //////////////////////////EDIT/////////////////////
+ const handleedit = () =>{
+   console.log('oh')
+ }
   return (
     <div className="App">
-      <br/>
-      <br/>
+     <div className="App">
+      <div>
       <div>
       <select onChange={e=>setselect(e.target.value)} class="form-select" aria-label="Default select example">
         <option selected>Filter</option>
@@ -75,68 +155,76 @@ function App() {
        </select>
       <button  onClick={submit}>ADD</button>
       </dive>
-      {select === undefined ?
-         <table>
-         <thead>
-           <tr>
-             <th>
-               text
-             </th>
-             <th>
-               date
-             </th>
-             <th>
-               Priority
-             </th>
-             <th>
-               number
-             </th>
-             <th>
-               Delete
-             </th>
-           </tr>
-         </thead>
-         <tbody>
-             {data!== undefined ?data.map((task,i)=>(
-               <tr key={i}>
-               <td>{task.text}</td>
-               <td>{task.date}</td>
-               <td>{task.priority}</td>
-               <td>{task._id}</td>
-               <td><button onClick={e=>handledelete(e,task._id)}>Delete</button></td>
-               </tr>
-             )):null}
-         </tbody>
-       </table>
-      :null}
-   
-      {select!== undefined ? data.filter(result=>result.priority === select).map((task,i)=>(
-        <tr key={i}>
-        <td>{task.text}</td>
-        <td>{task.date}</td>
-        <td>{task.priority}</td>
-        <td>{task._id}</td>
-        </tr>
-      )
-      ):null}
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {_.map(state, (data, key) => {
+          return(
+            <div key={key} className={"column"}>
+              <h3>{data.title}</h3>
+              <Droppable droppableId={key}>
+                {(provided, snapshot) => {
+                  return(
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={"droppable-col"}
+                    >
+                      {select === undefined ? data.items.map((el, index) => {
+                        return(
+                          <Draggable key={el._id} index={index} draggableId={el._id}>
+                            {(provided, snapshot) => {
+                              console.log(snapshot)
+                              return(
+                                <div
+                                  className={`item ${snapshot.isDragging && "dragging"}`}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {el.name}{el.text}
+                                </div>
+                              )
+                            }}
+                          </Draggable>
+                        )
+                      }):null}
 
+                      {select!== undefined ? data.items.filter(result=>result.priority === select).map((el, index) => {
+                        return(
+                          <Draggable key={el._id} index={index} draggableId={el._id}>
+                            {(provided, snapshot) => {
+                              console.log(snapshot)
+                              return(
+                                <div
+                                  className={`item ${snapshot.isDragging && "dragging"}`}
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                {el.text}
+                                {el.date}
+                                {el.priority}
+                                 
+                                </div>
+                              )
+                            }}
+                          </Draggable>
+                        )
+                      })
+
+                      :null}
+                      {provided.placeholder}
+                    </div>
+                  )
+                }}
+              </Droppable>
+            </div>
+          )
+        })}
+      </DragDropContext>
+    </div>
     </div>
   );
 }
 
 export default App;
-
-// var array = tasks.data
-// for(var i = 0; i <array.length;i++){
-// setState(prev => {
-//   return {
-//     ...prev,
-//     todo: {
-//       title: "Todo",
-//       items: [
-//         array[i],
-//         ...prev.todo.items
-//       ]
-//     }
-//   }
-// })}
